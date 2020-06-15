cf445c46695bb7488555f2abd20afe72a5aae3bd
Hirota <k.hirota0205@gmail.com>
2020-06-15 15:43:56 +0800

    To Github for vba challenge

---
+++ VBAStocks/Not used/vba-challenge.vba.vb
@@ -0,0 +1,176 @@
+Sub wholeprocess()
+Dim WS As Worksheet
+Application.ScreenUpdating = False
+For Each WS In Worksheets
+WS.Activate
+
+Call ticker
+Call yearchange
+Call greatest
+Next WS
+
+End Sub
+
+
+Sub ticker()
+Dim myDic As Object
+Dim buf As Variant
+Dim WS As Worksheet
+Dim lastrow As Long
+
+Dim i As Long
+Dim j As Long
+
+Dim count As Long
+
+
+Set myDic = CreateObject("Scripting.Dictionary")
+lastrow = Cells(Rows.count, 1).End(xlUp).Row
+
+i = 1
+For i = 1 To lastrow
+
+buf = Cells(i, 1).Value
+If Not myDic.Exists(buf) Then
+       myDic.Add buf, buf
+End If
+Next i
+
+items = myDic.items
+
+j = 0
+For j = 0 To myDic.count - 1
+Cells(j + 1, 9) = items(j)
+
+Next j
+
+Set myDic = Nothing
+
+
+
+
+End Sub
+
+
+Sub yearchange()
+Dim lastrowdata As Long
+Dim lastrowticker As Long
+Dim i As Long
+Dim j As Long
+Dim firstdate As Long
+Dim lastdate As Long
+Dim searchkey As String
+Dim firstvalue As Double
+Dim lastvalue As Double
+
+
+Columns("J:J").Select
+Selection.Style = "Comma"
+Columns("K:K").Select
+Selection.Style = "Percent"
+Columns("L:L").Select
+Selection.NumberFormat = "0"
+
+lastrowdata = Cells(Rows.count, 1).End(xlUp).Row
+lastrowticker = Cells(Rows.count, 9).End(xlUp).Row
+
+fisrtdate = Application.WorksheetFunction.Min(Range("B:B"))
+lastdate = Application.WorksheetFunction.Max(Range("B:B"))
+
+For i = 2 To lastrowticker
+searchkey = Cells(i, 9).Value
+
+j = 2
+Do While j < lastrowdata
+
+If Cells(j, 1) = searchkey And Cells(j, 2) = fisrtdate Then
+firstvalue = Cells(j, 3).Value
+Else
+End If
+
+If Cells(j, 1) = searchkey And Cells(j, 2) = lastdate Then
+lastvalue = Cells(j, 6).Value
+Else
+End If
+
+j = j + 1
+
+Loop
+
+Cells(i, 10).Value = lastvalue - firstvalue
+Cells(i, 11).Value = lastvalue / firstvalue - 1
+Cells(i, 12).Value = Application.WorksheetFunction.SumIfs(Range("G:G"), Range("A:A"), Cells(i, 9).Value)
+
+Next i
+
+Range("I1") = "Ticker"
+Range("J1") = "Yearly Change"
+Range("K1") = "Change%"
+Range("L1") = "Total Volume"
+
+Range(Cells(2, 10), Cells(lastrowticker, 10)).Select
+Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlGreaterEqual _
+        , Formula1:="=0"
+    Selection.FormatConditions(Selection.FormatConditions.count).SetFirstPriority
+    With Selection.FormatConditions(1).Interior
+        .PatternColorIndex = xlAutomatic
+        .Color = 5296274
+        .TintAndShade = 0
+    End With
+    Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlLess, _
+        Formula1:="=0"
+    Selection.FormatConditions(Selection.FormatConditions.count).SetFirstPriority
+    With Selection.FormatConditions(1).Interior
+        .PatternColorIndex = xlAutomatic
+        .Color = 255
+        .TintAndShade = 0
+    End With
+
+
+
+End Sub
+
+Sub greatest():
+Dim increase As Double
+Dim decrease As Double
+Dim total As Double
+Dim increaserow As Long
+Dim decreaserow As Long
+Dim totalrow As Double
+Dim searchkey1 As Variant
+Dim searchkey2 As Variant
+Dim searchkey3 As Variant
+
+
+Range("N2").Value = "Greatest % Increase"
+Range("N3").Value = "Greatest % Decrease"
+Range("N4").Value = "Greatest Total Volume"
+Range("O1").Value = "Ticker"
+Range("P1").Value = "Value"
+
+increase = Application.WorksheetFunction.Max(Range("K:K"))
+decrease = Application.WorksheetFunction.Min(Range("K:K"))
+total = Application.WorksheetFunction.Max(Range("L:L"))
+
+searchkey1 = Format(increase, "0%")
+increaserow = Range("K:K").Find(searchkey1, , xlValues).Row
+
+searchkey2 = Format(decrease, "0%")
+decreaserow = Range("K:K").Find(searchkey2, , xlValues).Row
+
+searchkey3 = total
+
+
+totalrow = Range("L:L").Find(searchkey3, , xlValues).Row
+
+Range("O2").Value = Cells(increaserow, 9).Value
+Range("P2").Value = increase
+Range("P2").Style = "percent"
+Range("O3").Value = Cells(decreaserow, 9).Value
+Range("P3").Value = decrease
+Range("P3").Style = "percent"
+Range("O4").Value = Cells(totalrow, 9).Value
+Range("P4").Value = total
+
+
+End Sub
---
+++ VBAStocks/Not used/vba-challenge3.vb
@@ -0,0 +1,183 @@
+Sub wholeprocess()
+Dim WS As Worksheet
+' to process everysheet
+For Each WS In Worksheets
+Application.ScreenUpdating = False
+WS.Activate
+
+' to summarise per ticker
+Call tickersummary
+
+' to get the greatest value
+Call greatest
+
+Next WS
+End Sub
+
+Sub tickersummary()
+Dim myDic As Object
+Dim lastrow As Long
+Dim i As Long
+Dim j As Long
+Dim k As Long
+Dim n As Long
+Dim keyticker As Variant
+Dim keyopen As Variant
+Dim keyclose As Variant
+Dim firstdate As Long
+Dim lastdate As Long
+Dim lastrowticker As Long
+
+' count the row of data
+lastrow = Cells(Rows.count, 1).End(xlUp).Row
+
+' sort the data by ticker and then by date
+ActiveSheet.Sort.SortFields.Add2 Key:=Range(Cells(2, 1), Cells(lastrow, 1)), _
+SortOn:=xlSortOnValues, Order:=xlAscending, DataOption:=xlSortNormal
+ActiveWorkbook.ActiveSheet.Sort.SortFields.Add2 Key:=Range(Cells(2, 2), Cells(lastrow, 2)), _
+SortOn:=xlSortOnValues, Order:=xlAscending, DataOption:=xlSortNormal
+With ActiveSheet.Sort
+        .SetRange Range(Cells(1, 1), Cells(lastrow, 9))
+        .Header = xlYes
+        .MatchCase = False
+        .Orientation = xlTopToBottom
+        .SortMethod = xlPinYin
+        .Apply
+End With
+
+' use dictionary to store the data
+Set myDic = CreateObject("Scripting.Dictionary")
+
+
+' process to each row
+' first day of each ticker should be the top because of the soring above. 
+' So if current row data is different from the data one above, it should be the first data of a ticker.
+' last data should be different from the data one below.
+
+i = 2
+j = 0
+k = 0
+For i = 2 To lastrow
+
+
+If Cells(i, 1).Value <> Cells(i - 1, 1).Value Then
+myDic.Add "ticker" & j, Cells(i, 1).Value
+myDic.Add "open" & j, Cells(i, 3).Value
+j = j + 1
+Else
+End If
+If Cells(i, 1).Value <> Cells(i + 1, 1).Value Then
+myDic.Add "close" & k, Cells(i, 6).Value
+k = k + 1
+Else
+End If
+Next i
+
+' extract data from dictionary for each ticker
+
+For n = 0 To j - 1
+keyticker = "ticker" & n
+keyopen = "open" & n
+keyclose = "close" & n
+
+Cells(n + 2, 9).Value = myDic.Item("ticker" & n)
+Cells(n + 2, 10).Value = myDic.Item("close" & n) - myDic.Item("open" & n)
+
+' If open price equal 0, divided cannot work. 
+If myDic.Item("open" & n) > 0 Then
+Cells(n + 2, 11).Value = (myDic.Item("close" & n) / myDic.Item("open" & n)) - 1
+Else
+Cells(n + 2, 11).Value = "N/A"
+End If
+
+' excel function sumifs  
+Cells(n + 2, 12).Value = Application.WorksheetFunction.SumIfs(Range("G:G"), Range("A:A"), Cells(n + 2, 9).Value)
+
+Next n
+
+Set myDic = Nothing
+
+' format settings
+Range("I1") = "Ticker"
+Range("J1") = "Yearly Change"
+Range("K1") = "Change%"
+Range("L1") = "Total Volume"
+
+Columns("J:J").Select
+Selection.Style = "Comma"
+Columns("K:K").Select
+Selection.Style = "Percent"
+Columns("L:L").Select
+Selection.NumberFormat = "0"
+lastrowticker = Cells(Rows.count, 9).End(xlUp).Row
+Range(Cells(2, 10), Cells(lastrowticker, 10)).Select
+Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions.Add Type:=xlCellValue, Operator:=xlGreaterEqual _
+        , Formula1:="=0"
+    Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions(Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions.count).SetFirstPriority
+    With Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions(1).Interior
+        .PatternColorIndex = xlAutomatic
+        .Color = 5296274
+        .TintAndShade = 0
+    End With
+    Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions.Add Type:=xlCellValue, Operator:=xlLess, _
+        Formula1:="=0"
+    Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions(Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions.count).SetFirstPriority
+    With Range(Cells(2, 10), Cells(lastrowticker, 10)).FormatConditions(1).Interior
+        .PatternColorIndex = xlAutomatic
+        .Color = 255
+        .TintAndShade = 0
+    End With
+
+
+
+End Sub
+
+
+
+Sub greatest():
+Dim increase As Double
+Dim decrease As Double
+Dim total As Double
+Dim increaserow As Long
+Dim decreaserow As Long
+Dim totalrow As Double
+Dim searchkey1 As Variant
+Dim searchkey2 As Variant
+Dim searchkey3 As Variant
+
+' header settings
+Range("N2").Value = "Greatest % Increase"
+Range("N3").Value = "Greatest % Decrease"
+Range("N4").Value = "Greatest Total Volume"
+Range("O1").Value = "Ticker"
+Range("P1").Value = "Value"
+
+' use excel function to get max, min
+increase = Application.WorksheetFunction.max(Range("K:K"))
+decrease = Application.WorksheetFunction.min(Range("K:K"))
+total = Application.WorksheetFunction.max(Range("L:L"))
+
+' identify the ticker number using find
+searchkey1 = Format(increase, "0%")
+increaserow = Range("K:K").Find(searchkey1, , xlValues).Row
+searchkey2 = Format(decrease, "0%")
+decreaserow = Range("K:K").Find(searchkey2, , xlValues).Row
+searchkey3 = total
+totalrow = Range("L:L").Find(searchkey3, , xlValues).Row
+
+' format settings / input the data 
+Range("O2").Value = Cells(increaserow, 9).Value
+Range("P2").Value = increase
+Range("P2").Style = "percent"
+Range("O3").Value = Cells(decreaserow, 9).Value
+Range("P3").Value = decrease
+Range("P3").Style = "percent"
+Range("O4").Value = Cells(totalrow, 9).Value
+Range("P4").Value = total
+Range("N:N").ColumnWidth = 20
+Range("P:P").ColumnWidth = 30
+
+End Sub
+
+
+
